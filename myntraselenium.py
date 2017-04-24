9cda5831c95d993c8d4cd4e1ef7f39de1ca32028
RanjithPai <mgranjith1@gmail.com>
2017-04-25 01:42:37 +0530

    Update myntraselenium.py
    moving to multiple pages

--- myntraselenium.py
+++ myntraselenium.py
@@ -1,9 +1,9 @@
 from scrapy import Spider
-
+from time import sleep
 from scrapy.selector import Selector
 from selenium import webdriver
 from scrapy.http import Request
-
+from selenium.common.exceptions import NoSuchElementException
 class NikeSpider(Spider):
 	name = "men"
         allowed_domains = ["myntra.com"]
@@ -18,6 +18,24 @@ class NikeSpider(Spider):
 		
 		for product in products:
 			url='http://www.myntra.com/'+product
-			yield Request(url,callback=self.parse_book) 
+			yield Request(url,callback=self.parse_book)
+		while True:
+			try:
+				next_page=self.driver.find_element_by_xpath('//div[@class="results-showmore"]')
+				sleep(3)
+				self.logger.info('Sleeping for 3 seconds')
+				next_page.click() 
+				sel=Selector(text=self.driver.page_source)
+				products=sel.xpath('//*[@class="product-base"]/a/@href').extract()
+
+		
+				for product in products:
+					url='http://www.myntra.com/'+product
+					yield Request(url,callback=self.parse_book)
+			except NoSuchElementException:
+				self.logger.info('No more elements to display')
+				self.driver.quit()
+				break
+		
 	def parse_book(self,response):
 		pass
