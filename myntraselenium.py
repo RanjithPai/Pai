999809e1182fe868aaa22a59dc381ff71106de71
RanjithPai <mgranjith1@gmail.com>
2017-04-24 16:56:43 +0530

    Create myntraselenium.py
    Myntra website is written in javascript and HTML
    Using scrapy only I could not extract the details of products
    Use selenium with scrapy
    Webdriver Firefox geckodriver

---
+++ myntraselenium.py
@@ -0,0 +1,15 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+from selenium import webdriver
+from scrapy.http import Request
+class NikeSpider(Spider):
+	name = "men"
+        allowed_domains = ["myntra.com"]
+        start_urls = ['www.myntra.com/men-tshirts?src=tNav']
+        
+        def start_requests(self):
+        	self.driver = webdriver.Firefox(executable_path='/home/biodiv/geckodriver')
+    		self.driver.get('http://www.myntra.com/men-tshirts?src=tNav')
+		sel=Selector(text=self.driver.page_source)
+		products=sel.xpath('//*[@class="product-base"]')
+		print products
