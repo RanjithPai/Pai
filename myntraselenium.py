45b0db63c6011512e42e9e7bf0a1a2714faa22ed
RanjithPai <mgranjith1@gmail.com>
2017-04-26 02:33:59 +0530

    Update myntraselenium.py
    to extract product details from single page

--- myntraselenium.py
+++ myntraselenium.py
@@ -4,38 +4,32 @@ from scrapy.selector import Selector
 from selenium import webdriver
 from scrapy.http import Request
 from selenium.common.exceptions import NoSuchElementException
-class NikeSpider(Spider):
+class ProductsSpider(Spider):
 	name = "men"
-        allowed_domains = ["myntra.com"]
-        start_urls = ['www.myntra.com/men-tshirts?src=tNav']
-        
-        def start_requests(self):
-        	self.driver = webdriver.Firefox(executable_path='/home/biodiv/geckodriver')
-    		self.driver.get('http://www.myntra.com/men-tshirts?src=tNav')
-		sel=Selector(text=self.driver.page_source)
-		products=sel.xpath('//*[@class="product-base"]/a/@href').extract()
-
+        allowed_domains = ["mynthra.com"]
+        start_urls = ['http://www.myntra.com/men-tshirts?src=tNav']
+	def parse(self, response):
+		driver = webdriver.Firefox(executable_path='/home/biodiv/geckodriver')
+	    	driver.get('http://www.myntra.com/men-tshirts?src=tNav')	
 		
+	       
+			
+		sel=Selector(text=driver.page_source)
+		products=sel.xpath('//*[@class="product-base"]/a')
+		
+		#print (products)
 		for product in products:
-			url='http://www.myntra.com/'+product
-			yield Request(url,callback=self.parse_book)
-		while True:
-			try:
-				next_page=self.driver.find_element_by_xpath('//div[@class="results-showmore"]')
-				sleep(3)
-				self.logger.info('Sleeping for 3 seconds')
-				next_page.click() 
-				sel=Selector(text=self.driver.page_source)
-				products=sel.xpath('//*[@class="product-base"]/a/@href').extract()
+			brand=product.xpath('.//*[@class="product-brand"]/text()').extract()
+			description=product.xpath('.//*[@class="product-product"]/text()').extract()
+			actual_price=(product.xpath('.//*[@class="product-strike"]/text()').extract() or product.xpath('.//*[@class="product-price"]/span/text()').extract()) 
+			
+				
+			discount_price=(product.xpath('.//*[@class="product-discountedPrice"]/text()').extract() or product.xpath('.//*[@class="product-price"]/span/text()').extract())
+			discount_percentage=product.xpath('.//*[@class="product-discountPercentage"]/text()').extract()
 
-		
-				for product in products:
-					url='http://www.myntra.com/'+product
-					yield Request(url,callback=self.parse_book)
-			except NoSuchElementException:
-				self.logger.info('No more elements to display')
-				self.driver.quit()
-				break
-		
-	def parse_book(self,response):
-		pass
+			yield{
+			'brand':brand,
+			'Description':description,
+			'Actual_price':actual_price,
+			'Discount_price':discount_price,
+			'Discount_percentage':discount_percentage}
